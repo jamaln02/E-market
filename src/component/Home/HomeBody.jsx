21d2b1cd091c2420ea28b5b63b6b4adab2e77481
jamaln02 <152226076+jamaln02@users.noreply.github.com>
2024-05-28 19:56:30 +0300

    design fix

--- src/component/Home/HomeBody.jsx
+++ src/component/Home/HomeBody.jsx
@@ -3,7 +3,7 @@ import ProductsCard from "./ProductsCard";
 
 const HomeBody = () => {
   return (
-    <div className="mt-20 p-5 text-gray-500 dark:text-gray-600">
+    <div className="mt-14 p-5 text-gray-500 dark:text-gray-600">
       <div className=" bg-gray-300 dark:bg-gray-400 shadow-2xl border-s-8 border-main">
         <h1 className="text-lg md:text-xl lg:text-2xl p-2 font-bold">
           SEE OUR PRODUCTS
